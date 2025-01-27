1994a1dbc85141dc218189e67cdffc3ee7ebb2f9
Sa-Ev <sa-ev@gmx.net>
2025-01-27 22:59:28 +0100

    Add HW-1 project

---
+++ HW-1/app.js
@@ -0,0 +1,6 @@
+const { logMessage } = require("./simpleLogger/logger");
+
+// Записываем несколько тестовых сообщений
+logMessage("This message was created");
+logMessage("This message is edited");
+logMessage("This message successfully transmitted");
